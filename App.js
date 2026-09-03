df67c7dbdb4b29000351910088f86fc93d558e8b
Yasin <yasinhassan004@gamil.com>
2026-09-03 21:30:13 +0300

    fix: some screens not rendering

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { AuthProvider } from "./src/context/AuthContext";
+import { ToastProvider } from "./src/context/ToastContext";
 import RootNavigator from "./src/navigation/RootNavigator";
 
 export default function App() {
   return (
-    <AuthProvider>
-      <StatusBar style="dark" />
-      <RootNavigator />
-    </AuthProvider>
+    <ToastProvider>
+      <AuthProvider>
+        <StatusBar style="dark" />
+        <RootNavigator />
+      </AuthProvider>
+    </ToastProvider>
   );
 }
